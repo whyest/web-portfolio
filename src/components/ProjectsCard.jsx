b49a4d025d1127f07ccf664bbba8fefc66db3f51
Денис Галкин <whyest@gmail.com>
2023-09-28 22:20:39 +0300

    fix links in projects card

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -2,7 +2,7 @@ import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa'
 import { TbWorldWww } from 'react-icons/tb'
 const ProjectsCard = ({ url, img, github, title, text }) => {
 	return (
-		<article className='bg-white dark:bg-slate-700 rounded-lg shadow-md block hover:shadow-xl duration-300'>
+		<article className='relative bg-white pb-10 dark:bg-slate-700 rounded-lg shadow-md block hover:shadow-xl duration-300'>
 			<img
 				src={img}
 				alt={title}
@@ -15,7 +15,7 @@ const ProjectsCard = ({ url, img, github, title, text }) => {
 				<p className='mt-4 text-grayishBlue dark:text-emerald-50 leading-loose'>
 					{text}
 				</p>
-				<div className='mt-6 flex gap-x-4'>
+				<div className='absolute bottom-8 left-8 mt-6 flex gap-x-4'>
 					<a href={url} target='_blank'>
 						<TbWorldWww className='h-8 w-8 text-grayishBlue hover:text-emerald-600 hover:dark:text-emerald-500 dark:text-emerald-100 duration-300' />
 					</a>
