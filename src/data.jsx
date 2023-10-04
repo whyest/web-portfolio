89a901eef4a868fcec81717e1eec1bc772b2fdf0
Денис Галкин <whyest@gmail.com>
2023-10-04 21:31:00 +0300

    add three more projects to web creations section

--- src/data.jsx
+++ src/data.jsx
@@ -3,6 +3,9 @@ import { FaHtml5, FaJs, FaReact } from 'react-icons/fa'
 import project1 from './assets/project-1.jpg'
 import project2 from './assets/project-2.png'
 import project3 from './assets/project-3.jpg'
+import project4 from './assets/project-4.jpg'
+import project5 from './assets/project-5.jpg'
+import project6 from './assets/project-6.jpg'
 
 export const links = [
 	{ id: nanoid(), href: '#home', text: 'home' },
@@ -35,10 +38,26 @@ export const skills = [
 export const projects = [
 	{
 		id: nanoid(),
+		img: project5,
+		url: 'https://fancy-estore.netlify.app/',
+		github: 'https://github.com/whyest/fancy-store',
+		title: 'Fancy Logo E-commerce App',
+		text: 'A project of the e-commerce using ReactJS, Styled Components and React Router. The website is fully responsive.',
+	},
+	{
+		id: nanoid(),
+		img: project4,
+		url: 'https://whyest.github.io/sitdownpls-deploy/',
+		github: 'https://github.com/whyest/sitdownpls',
+		title: 'E-commerce «SitDownPls»',
+		text: 'A project of the e-commerce using HTML/CSS, SCSS and Gulp bundler. The website is fully responsive.',
+	},
+	{
+		id: nanoid(),
 		img: project1,
 		url: 'https://styled-comp-project.netlify.app',
 		github: 'https://github.com/whyest/styled-comp-project',
-		title: 'Fancy Logo React Website',
+		title: 'Fancy Logo React Landing Page',
 		text: 'The website is fully responsive. The main sections are reusable components. It includes ReactJS, Styled Components, React Router 6.',
 	},
 	{
@@ -56,5 +75,14 @@ export const projects = [
 		github: 'https://github.com/whyest/mortgage-calculator',
 		title: 'Mortgage Calculator',
 		text: 'A project of the mortgage calculator using ReactJS, Material UI and ChartJS.',
+	},
+
+	{
+		id: nanoid(),
+		img: project6,
+		url: 'https://whyest.github.io/blanchard/',
+		github: 'https://github.com/whyest/blanchard#blanchard',
+		title: 'Blanchard - art gallery',
+		text: 'A project of the landing page using HTML/CSS and JavaScript. The website is fully responsive.',
 	},
 ]
